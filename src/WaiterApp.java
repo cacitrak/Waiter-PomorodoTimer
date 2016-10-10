be3923ec4f4be703037b2711262ae46fcccb576b
Cody Citrak <cdyctrk@gmail.com>
2016-10-10 11:40:35 -0400

    Build 1
    Working bare bones app.
    Loading bars load as the time passes

--- src/WaiterApp.java
+++ src/WaiterApp.java
@@ -1,6 +1,11 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -10,6 +15,7 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class WaiterApp extends Application {
 	
@@ -17,7 +23,22 @@ public class WaiterApp extends Application {
 	private int DEFAULT_WIDTH = 360;
 	private int currentStep = 0;
 	
+	//1500 - work time
+	//300 pause time
+	//900 long pause
+	
+	private int workTime =  5; //Seconds
+	private int pauseTime = 3;
+	private int longPauseTime = 4;
+	private Integer timeSeconds = 0;
+	
+	public Timeline timeline;
+	public Text timerText;
+	
+	Map<Integer, PomodoroPhase> phaseByStep = new HashMap<>();
 	ArrayList<Rectangle> loadingBars = new ArrayList<>();
+	
+	int fullBarWidth = 250;
 	
 	public static void main(String[] args) {
 		launch(args);
@@ -33,8 +54,27 @@ public class WaiterApp extends Application {
 		primaryStage.setScene(scene);
 		addTimerBarsToGroup(root);
 		addStartButton(root);
+		addTimerText(root);
+		
+		phaseByStep.put(0, PomodoroPhase.WORK);
+		phaseByStep.put(1, PomodoroPhase.PAUSE);
+		phaseByStep.put(2, PomodoroPhase.WORK);
+		phaseByStep.put(3, PomodoroPhase.PAUSE);
+		phaseByStep.put(4, PomodoroPhase.WORK);
+		phaseByStep.put(5, PomodoroPhase.PAUSE);
+		phaseByStep.put(6, PomodoroPhase.WORK);
+		phaseByStep.put(7, PomodoroPhase.LONGPAUSE);
 		
 		primaryStage.show();
+	}
+	
+	private void addTimerText(Group group) {
+		timerText = new Text("00:00:00");
+		timerText.setX(150);
+		timerText.setY(100);
+		timerText.setFont(new Font("Verdana", 20));
+		
+		group.getChildren().add(timerText);
 	}
 	
 	private void addStartButton(Group group) {
@@ -54,18 +94,18 @@ public class WaiterApp extends Application {
 		text.setY(yPos + height/2);
 		
 		start.setOnMouseClicked(new EventHandler<MouseEvent>() {
-
 			@Override
 			public void handle(MouseEvent arg0) {
 				System.out.println("** Start button clicked ***");
+				startTimer();
 			}
 		});
 		
 		text.setOnMouseClicked(new EventHandler<MouseEvent>() {
-
 			@Override
 			public void handle(MouseEvent arg0) {
 				System.out.println("** Start Text clicked ***");
+				startTimer();
 			}
 		});
 		
@@ -88,7 +128,7 @@ public class WaiterApp extends Application {
 			
 			Text text;
 			
-			Rectangle lBar = new Rectangle(0, height);
+			Rectangle lBar = new Rectangle(25, height);
 			lBar.setX(xPos);
 			lBar.setY(yPos);
 			lBar.setOpacity(.5);
@@ -107,8 +147,7 @@ public class WaiterApp extends Application {
 				text.setY(yPos + height);
 
 				yPos += height;
-			}
-			else { //Break timer bar
+			} else { //Break timer bar
 				bar.setStroke(Color.DARKBLUE);
 				bar.setFill(Color.LAVENDER);
 				
@@ -128,5 +167,91 @@ public class WaiterApp extends Application {
 			group.getChildren().addAll(bar, text);
 			group.getChildren().add(lBar);
 		}
+	}
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public void startTimer() {
+		if (timeline != null) timeline.stop();
+		else timeline = new Timeline();
+		
+		timeSeconds = workTime;
+		timeline.setCycleCount(workTime);
+		timeline.getKeyFrames().add(
+				new KeyFrame(Duration.seconds(1), 
+						new EventHandler() {
+					@Override
+					public void handle(Event event) {
+						timeSeconds--;
+						timerText.setText(getRemainingTime());
+						handleLoadingBar();
+						
+						if (timeSeconds <= 0) {
+							timeline.stop();
+							int nextPhase = getNextPhaseTime();
+							timeline.setCycleCount(nextPhase);
+							timeSeconds = nextPhase;
+							timeline.playFromStart();
+						}
+					}
+				}));
+		timeline.playFromStart();
+	}
+	
+	public void handleLoadingBar() {
+		int currPhaseTime = getCurrentPhaseTime();
+		int elapsedTime = currPhaseTime - timeSeconds;
+		
+		double fractionElapsed = (double)elapsedTime/(double)currPhaseTime;
+		System.out.println(currPhaseTime + ", " + elapsedTime + ", " + fractionElapsed);
+		
+		loadingBars.get(currentStep).setWidth(fullBarWidth * fractionElapsed);
+	}
+	
+	public int getCurrentPhaseTime()
+	{
+		switch(phaseByStep.get(currentStep)) {
+		case WORK:
+			return workTime;
+		case PAUSE:
+			return pauseTime;
+		case LONGPAUSE:
+			return longPauseTime;
+		}
+		return 0;
+	}
+	
+	public int getNextPhaseTime() {
+		currentStep += 1;
+		if(currentStep > 7) { 
+			currentStep = 0;
+			for(Rectangle r : loadingBars)
+				r.setWidth(0);
+		}
+		
+		switch(phaseByStep.get(currentStep)) {
+		case WORK:
+			return workTime;
+		case PAUSE:
+			return pauseTime;
+		case LONGPAUSE:
+			return longPauseTime;
+		}
+		return 0;
+	}
+	
+	public String getRemainingTime() {
+		int tempRem = timeSeconds;
+		int hours = timeSeconds / 3600;
+		tempRem = tempRem % 3600;
+		int minutes = tempRem / 60;
+		tempRem = tempRem % 60;
+		
+		return hours + ":" + minutes + ":" + tempRem;
+	}
+	
+	enum PomodoroPhase {
+		WORK,
+		PAUSE,
+		LONGPAUSE
 	}
 }
