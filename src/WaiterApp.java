ad7924eba0641ad46c09721c7401117a026e154f
Cody Citrak <cdyctrk@gmail.com>
2017-03-12 21:05:49 -0400

    Play/Pause/Reset funtionality complete

--- src/WaiterApp.java
+++ src/WaiterApp.java
@@ -3,37 +3,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 import app.PomodoroPhase;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.ProgressBar;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 public class WaiterApp extends Application {
 	
-	private int DEFAULT_HEIGHT = 450;
-	private int DEFAULT_WIDTH = 380;
-	private int currentStep = 0;
-	
-	/** To see the progress bars step, decrease the 3 time variables below */
-	private int workTime =  1500; //Seconds
-	private int pauseTime = 300;
-	private int longPauseTime = 900;
-	private Integer timeSeconds = 0;
-	
-	public Timeline timeline;
 	public Text timerText;
 	
 	Map<Integer, PomodoroPhase> phaseByStep = new HashMap<>();
@@ -54,181 +34,6 @@ public class WaiterApp extends Application {
 		
 		Scene scene = new Scene(root);
 		primaryStage.setScene(scene);
-		/*
-		primaryStage.setHeight(DEFAULT_HEIGHT);
-		primaryStage.setWidth(DEFAULT_WIDTH);
-		Group root = new Group();
-		Scene scene = new Scene(root);
-		primaryStage.setScene(scene);
-		
-		Rectangle bg = new Rectangle(DEFAULT_WIDTH, DEFAULT_HEIGHT, Color.ALICEBLUE);
-		root.getChildren().add(bg);
-		
-		addTimerBarsToGroup(root);
-		addStartButton(root);
-		addTimerText(root);
-		
-		phaseByStep.put(0, PomodoroPhase.WORK);
-		phaseByStep.put(1, PomodoroPhase.PAUSE);
-		phaseByStep.put(2, PomodoroPhase.WORK);
-		phaseByStep.put(3, PomodoroPhase.PAUSE);
-		phaseByStep.put(4, PomodoroPhase.WORK);
-		phaseByStep.put(5, PomodoroPhase.PAUSE);
-		phaseByStep.put(6, PomodoroPhase.WORK);
-		phaseByStep.put(7, PomodoroPhase.LONGPAUSE);
-		*/
 		primaryStage.show();
-	}
-	
-	private void addTimerText(Group group) {
-		timerText = new Text("00:00:00");
-		timerText.setX(150);
-		timerText.setY(60);
-		timerText.setFont(new Font("Verdana", 20));
-		
-		group.getChildren().add(timerText);
-	}
-	
-	private void addStartButton(Group group) {
-		int height = 70;
-		int width = 70;
-		int xPos = 40;
-		int yPos = 20;
-		
-		Circle start = new Circle(70, 50, width/2, Color.LIGHTSTEELBLUE);
-		
-		Text text = new Text("Start");
-		text.setFont(Font.font("Verdana", 20));
-		text.setX(xPos);
-		text.setY(yPos + height/2);
-		
-		start.setOnMouseClicked(new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent arg0) {
-				startTimer();
-			}
-		});
-		
-		text.setOnMouseClicked(new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent arg0) {
-				startTimer();
-			}
-		});
-		
-		group.getChildren().addAll(start, text);
-	}
-	
-	private void addTimerBarsToGroup(Group group) {
-		int height = 30;
-		int width = 300;
-		
-		int verticalOffset = 10;
-		int xPos= 20;
-		int yPos = 100;
-		
-		for(int i = 0; i < 8; i++) {
-			
-			ProgressBar pb = new ProgressBar(0);
-			pb.setLayoutX(xPos);
-			pb.setLayoutY(yPos);
-			pb.setPrefHeight(height);
-			pb.setPrefWidth(width);
-			
-			Text indic = new Text();
-			indic.setFont(Font.font("Comic Sans", 20));
-			indic.setX(xPos + width + 5);
-			indic.setY(yPos + 21);
-			
-			if(i % 2 == 0) { // Work timer bar
-				indic.setText("W");
-				yPos += height;
-			} else { //Break timer bar
-				indic.setText("P");
-				indic.setX(xPos + width + 7);
-				yPos = yPos + height + verticalOffset;
-			}
-			
-			loadingBars.add(pb);
-			group.getChildren().add(pb);
-			group.getChildren().add(indic);
-		}
-	}
-	
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public void startTimer() {
-		if (timeline != null) timeline.stop();
-		else timeline = new Timeline();
-		
-		timeSeconds = workTime;
-		timeline.setCycleCount(workTime);
-		timeline.getKeyFrames().add(
-				new KeyFrame(Duration.seconds(1), 
-						new EventHandler() {
-					@Override
-					public void handle(Event event) {
-						timeSeconds--;
-						timerText.setText(getRemainingTime());
-						handleLoadingBar();
-						
-						if (timeSeconds <= 0) {
-							timeline.stop();
-							int nextPhase = getNextPhaseTime();
-							timeline.setCycleCount(nextPhase);
-							timeSeconds = nextPhase;
-							timeline.playFromStart();
-						}
-					}
-				}));
-		timeline.playFromStart();
-	}
-	
-	public void handleLoadingBar() {
-		int currPhaseTime = getCurrentPhaseTime();
-		int elapsedTime = currPhaseTime - timeSeconds;
-		
-		double fractionElapsed = (double)elapsedTime/(double)currPhaseTime;
-		
-		loadingBars.get(currentStep).setProgress(fractionElapsed);
-	}
-	
-	public int getCurrentPhaseTime()
-	{
-		switch(phaseByStep.get(currentStep)) {
-		case WORK:		return workTime;
-		case PAUSE:	    return pauseTime;
-		case LONGPAUSE:	return longPauseTime;
-		}
-		return 0;
-	}
-	
-	public int getNextPhaseTime() {
-		currentStep += 1;
-		if(currentStep > 7) { 
-			currentStep = 0;
-			for(ProgressBar r : loadingBars)
-				r.setProgress(0);
-		}
-		
-		switch(phaseByStep.get(currentStep)) {
-		case WORK:
-			return workTime;
-		case PAUSE:
-			return pauseTime;
-		case LONGPAUSE:
-			return longPauseTime;
-		}
-		return 0;
-	}
-	
-	public String getRemainingTime() {
-		int tempRem = timeSeconds;
-		String hours = String.format("%02d", timeSeconds / 3600);
-		tempRem = tempRem % 3600;
-		String minutes = String.format("%02d", tempRem / 60);
-		tempRem = tempRem % 60;
-		String seconds = String.format("%02d", tempRem);
-		
-		return hours + ":" + minutes + ":" + seconds;
 	}
 }
--- src/gui/WaiterController.java
+++ src/gui/WaiterController.java
@@ -1,14 +1,22 @@
 package gui;
 
 import java.net.URL;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
-import PomodoroPhase;
+import app.PomodoroPhase;
 import fonts.AwesomeIcons;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.PopupControl;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.control.Tooltip;
+import javafx.util.Duration;
 
 public class WaiterController implements Initializable {
 
@@ -28,28 +36,182 @@ public class WaiterController implements Initializable {
 	@FXML private ProgressBar pause3;
 	@FXML private ProgressBar pause4;
 	
-	private int workTime;
-	private int shortPauseTime;
-	private int longPauseTime;
+	private HashMap<String, ProgressBar> barMap;
+	
+	private int workTime = 10; 
+			//1500;
+	private int shortPauseTime = 5;
+	//300;
+	private int longPauseTime = 8;
+	//900;
 	
 	private app.PomodoroPhase currentPhase = app.PomodoroPhase.WORK;
+	private int currentStep = 1;
+	
+	private Timeline timeline;
+	private Integer timeSeconds = 0;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		startPause.setText(AwesomeIcons.ICON_PLAY);
+		Tooltip.install(startPause, new Tooltip("Start/Pause the timer"));
 		restart.setText(AwesomeIcons.ICON_REFRESH);
+		Tooltip.install(restart, new Tooltip("Reset the timer completely"));
 		back.setText(AwesomeIcons.ICON_STEP_BACKWARD);
+		Tooltip.install(back, new Tooltip("Step backward one phase"));
 		forward.setText(AwesomeIcons.ICON_STEP_FORWARD);
+		Tooltip.install(forward, new Tooltip("Step forward one phase"));
 		
 		timerText.setText("00:00:00");
+		
+		barMap = new HashMap<>();
+		barMap.put(PomodoroPhase.WORK.name() + 1, work1);
+		barMap.put(PomodoroPhase.WORK.name() + 2, work2);
+		barMap.put(PomodoroPhase.WORK.name() + 3, work3);
+		barMap.put(PomodoroPhase.WORK.name() + 4, work4);
+		
+		barMap.put(PomodoroPhase.PAUSE.name() + 1, pause1);
+		barMap.put(PomodoroPhase.PAUSE.name() + 2, pause2);
+		barMap.put(PomodoroPhase.PAUSE.name() + 3, pause3);
+		barMap.put(PomodoroPhase.LONGPAUSE.name() + 4, pause4);
 	}
 	
 	@FXML
 	private void startPauseMouseClick() {
-		if(AwesomeIcons.ICON_PLAY.equals(startPause.getText()))
-				startPause.setText(AwesomeIcons.ICON_PAUSE);
-		else
-				startPause.setText(AwesomeIcons.ICON_PLAY);
+		if(AwesomeIcons.ICON_PLAY.equals(startPause.getText())){
+			startPause.setText(AwesomeIcons.ICON_PAUSE);
+			startTimer();
+		}
+		else {
+			startPause.setText(AwesomeIcons.ICON_PLAY);
+			pauseTimer();
+		}
 	}
+	
+	private ProgressBar currentPhase() {
+		return barMap.get(currentPhase.name() + currentStep);
+	}
+	
+	public void startTimer() {
+		if (timeline != null) timeline.play();
+		else {
+			timeline = new Timeline();
 
+			timeSeconds = workTime;
+			timeline.setCycleCount(workTime);
+			timeline.getKeyFrames().add(
+					new KeyFrame(Duration.seconds(1), 
+							new EventHandler() {
+						@Override
+						public void handle(Event event) {
+							timeSeconds--;
+							timerText.setText(getRemainingTime());
+							handleLoadingBar();
+
+							if (timeSeconds <= 0) {
+								timeline.stop();
+								prepNextPhase();
+								timeline.setCycleCount(timeSeconds);
+								timeline.playFromStart();
+							}
+						}
+					}));
+			timeline.playFromStart();
+		}
+	}
+	
+	public void pauseTimer() {
+		timeline.pause();
+	}
+	
+	@FXML
+	public void resetTimer() {
+		prepFirstPhase();
+	}
+	
+	private void prepNextPhase() {
+		PomodoroPhase nextPhase = nextPhaseType();
+		if(nextPhase == PomodoroPhase.WORK) {
+			currentStep++;
+			if(currentStep > 4) {
+				currentStep = 1;
+				resetBars();
+			}
+		}
+		timeSeconds = nextPhaseTime();
+		currentPhase = nextPhase;
+	}
+	
+	private void prepFirstPhase() {
+		if(timeline != null){
+			timeline.stop();
+			currentPhase = PomodoroPhase.WORK;
+			currentStep = 1;
+			timeSeconds = workTime;
+			timeline.setCycleCount(workTime);
+			resetBars();
+			timerText.setText("00:00:00");
+			if(AwesomeIcons.ICON_PAUSE.equals(startPause.getText()))
+				startPause.setText(AwesomeIcons.ICON_PLAY);
+		}
+	}
+	
+	public void handleLoadingBar() {
+		int currPhaseTime = currentPhaseTime();
+		int elapsedTime = currPhaseTime - timeSeconds;
+		
+		double fractionElapsed = (double)elapsedTime/(double)currPhaseTime;
+		
+		currentPhase().setProgress(fractionElapsed);
+	}
+	
+	public int currentPhaseTime()
+	{
+		switch(currentPhase) {
+		case WORK:		return workTime;
+		case PAUSE:	    return shortPauseTime;
+		case LONGPAUSE:	return longPauseTime;
+		}
+		return -1;
+	}
+	
+	private PomodoroPhase nextPhaseType() {
+		switch(currentPhase) {
+		case WORK:
+			if(currentStep == 4) return PomodoroPhase.LONGPAUSE;
+			return PomodoroPhase.PAUSE;
+		case PAUSE:
+		case LONGPAUSE:
+			return PomodoroPhase.WORK;
+		}
+		return null;
+	}
+	
+	public int nextPhaseTime() {
+		switch(currentPhase) {
+		case WORK:
+			if(currentStep == 4) return longPauseTime;
+			return shortPauseTime;
+		case PAUSE:
+		case LONGPAUSE:
+			return workTime;
+		}
+		return -1;
+	}
+	
+	public String getRemainingTime() {
+		int tempRem = timeSeconds;
+		String hours = String.format("%02d", timeSeconds / 3600);
+		tempRem = tempRem % 3600;
+		String minutes = String.format("%02d", tempRem / 60);
+		tempRem = tempRem % 60;
+		String seconds = String.format("%02d", tempRem);
+		
+		return hours + ":" + minutes + ":" + seconds;
+	}
+	
+	private void resetBars() {
+		for(ProgressBar r : barMap.values())
+			r.setProgress(0);
+	}
 }
