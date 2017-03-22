e81b5dbde31492b53c9768e9d455e536d1c2307f
Cody Citrak <cdyctrk@gmail.com>
2017-03-21 21:51:26 -0400

    Window size shrink, progressbar indeterminate
    Adjust the size of the window to cut back on whitespace.
    When the progress bars are below roughly 0.04, they dont seem to
    show any progress. When the fractionElapsed is less than .04, bar
    is set to negative progress to indicate indeterminate status.

--- src/gui/WaiterController.java
+++ src/gui/WaiterController.java
@@ -24,29 +24,29 @@ public class WaiterController implements Initializable {
 	@FXML private Label back;
 	@FXML private Label forward;
 	@FXML private Label timerText;
-	
+
 	@FXML private ProgressBar work1;
 	@FXML private ProgressBar work2;
 	@FXML private ProgressBar work3;
 	@FXML private ProgressBar work4;
-	
+
 	@FXML private ProgressBar pause1;
 	@FXML private ProgressBar pause2;
 	@FXML private ProgressBar pause3;
 	@FXML private ProgressBar pause4;
-	
+
 	private HashMap<String, ProgressBar> barMap;
-	
+
 	private int workTime = 1500;
 	private int shortPauseTime = 300;
 	private int longPauseTime = 900;
-	
+
 	private app.PomodoroPhase currentPhase = app.PomodoroPhase.WORK;
 	private int currentStep = 1;
-	
+
 	private Timeline timeline;
 	private Integer timeSeconds = 0;
-	
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		startPause.setText(AwesomeIcons.ICON_PLAY);
@@ -57,20 +57,20 @@ public class WaiterController implements Initializable {
 		Tooltip.install(back, new Tooltip("Step backward one phase"));
 		forward.setText(AwesomeIcons.ICON_STEP_FORWARD);
 		Tooltip.install(forward, new Tooltip("Step forward one phase"));
-		
-		resetTimerText();		
+
+		resetTimerText();
 		barMap = new HashMap<>();
 		barMap.put(PomodoroPhase.WORK.name() + 1, work1);
 		barMap.put(PomodoroPhase.WORK.name() + 2, work2);
 		barMap.put(PomodoroPhase.WORK.name() + 3, work3);
 		barMap.put(PomodoroPhase.WORK.name() + 4, work4);
-		
+
 		barMap.put(PomodoroPhase.PAUSE.name() + 1, pause1);
 		barMap.put(PomodoroPhase.PAUSE.name() + 2, pause2);
 		barMap.put(PomodoroPhase.PAUSE.name() + 3, pause3);
 		barMap.put(PomodoroPhase.LONGPAUSE.name() + 4, pause4);
 	}
-	
+
 	@FXML private void startPauseMouseClick() {
 		if(AwesomeIcons.ICON_PLAY.equals(startPause.getText())){
 			startPause.setText(AwesomeIcons.ICON_PAUSE);
@@ -81,11 +81,11 @@ public class WaiterController implements Initializable {
 			pauseTimer();
 		}
 	}
-	
+
 	private ProgressBar currentPhase() {
 		return barMap.get(currentPhase.name() + currentStep);
 	}
-	
+
 	public void startTimer() {
 		if (timeline != null) timeline.play();
 		else {
@@ -94,7 +94,7 @@ public class WaiterController implements Initializable {
 			timeSeconds = currentPhaseTime();
 			timeline.setCycleCount(timeSeconds);
 			timeline.getKeyFrames().add(
-					new KeyFrame(Duration.seconds(1), 
+					new KeyFrame(Duration.seconds(1),
 							new EventHandler() {
 						@Override
 						public void handle(Event event) {
@@ -113,30 +113,30 @@ public class WaiterController implements Initializable {
 			timeline.playFromStart();
 		}
 	}
-	
+
 	public void pauseTimer() {
 		timeline.pause();
 	}
-	
+
 	@FXML public void resetTimer() {
 		prepFirstPhase();
 	}
-	
+
 	@FXML public void stepForwardPhase() {
 		if(timeline != null) {
 			timeline.stop();
-		} 
+		}
 		completeCurrentPhase();
 		prepNextPhase();
 	}
-	
+
 	@FXML public void stepBackPhase() {
 		if(timeline != null) {
 			timeline.stop();
 		}
 		prepPreviousPhase();
 	}
-	
+
 	private void prepNextPhase() {
 		PomodoroPhase nextPhase = nextPhaseType();
 		if(nextPhase == PomodoroPhase.WORK) {
@@ -150,7 +150,7 @@ public class WaiterController implements Initializable {
 		currentPhase = nextPhase;
 		resetTimerText();
 	}
-	
+
 	private void prepPreviousPhase() {
 		if(currentStep != 1 || (currentStep == 1 && currentPhase != PomodoroPhase.WORK)){
 			PomodoroPhase prevPhase = prevPhaseType();
@@ -163,7 +163,7 @@ public class WaiterController implements Initializable {
 			resetTimerText();
 		}
 	}
-	
+
 	private PomodoroPhase prevPhaseType() {
 		switch(currentPhase) {
 		case WORK:
@@ -177,7 +177,7 @@ public class WaiterController implements Initializable {
 		}
 		return PomodoroPhase.WORK;
 	}
-	
+
 	private void prepFirstPhase() {
 		if(timeline != null){
 			timeline.stop();
@@ -191,16 +191,17 @@ public class WaiterController implements Initializable {
 				startPause.setText(AwesomeIcons.ICON_PLAY);
 		}
 	}
-	
+
 	public void handleLoadingBar() {
 		int currPhaseTime = currentPhaseTime();
 		int elapsedTime = currPhaseTime - timeSeconds;
-		
+
 		double fractionElapsed = (double)elapsedTime/(double)currPhaseTime;
-		
+		//setting the bar to indeterminate here. For some reason nothing displays on the bar until about 0.04
+		if(fractionElapsed < 0.03) fractionElapsed = -1;
 		currentPhase().setProgress(fractionElapsed);
 	}
-	
+
 	public int currentPhaseTime()
 	{
 		switch(currentPhase) {
@@ -210,7 +211,7 @@ public class WaiterController implements Initializable {
 		}
 		return -1;
 	}
-	
+
 	private PomodoroPhase nextPhaseType() {
 		switch(currentPhase) {
 		case WORK:
@@ -222,7 +223,7 @@ public class WaiterController implements Initializable {
 		}
 		return null;
 	}
-	
+
 	public int nextPhaseTime() {
 		switch(currentPhase) {
 		case WORK:
@@ -234,7 +235,7 @@ public class WaiterController implements Initializable {
 		}
 		return -1;
 	}
-	
+
 	public String getRemainingTime() {
 		int tempRem = timeSeconds;
 		String hours = String.format("%02d", timeSeconds / 3600);
@@ -242,20 +243,20 @@ public class WaiterController implements Initializable {
 		String minutes = String.format("%02d", tempRem / 60);
 		tempRem = tempRem % 60;
 		String seconds = String.format("%02d", tempRem);
-		
+
 		return hours + ":" + minutes + ":" + seconds;
 	}
-	
+
 	private void resetBars() {
 		for(ProgressBar r : barMap.values())
 			r.setProgress(0);
 	}
-	
+
 	private void completeCurrentPhase() {
 		currentPhase().setProgress(1);
 		if(timeline != null) timeline.pause();
 	}
-	
+
 	private void resetTimerText() {
 		timerText.setText("00:00:00");
 	}
