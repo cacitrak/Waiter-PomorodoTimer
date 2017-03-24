9e3fa89ddee7ca92c7fd178e8b534c16579b3798
Cody Citrak <cdyctrk@gmail.com>
2017-03-23 21:53:30 -0400

    Application icons added
    Credit to http://codepen.io/sam-sas/ for making the image for me!

--- src/WaiterApp.java
+++ src/WaiterApp.java
@@ -7,31 +7,33 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class WaiterApp extends Application {
-	
+
 	public Text timerText;
-	
+
 	Map<Integer, PomodoroPhase> phaseByStep = new HashMap<>();
 	ArrayList<ProgressBar> loadingBars = new ArrayList<>();
-	
+
 	int fullBarWidth = 300;
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		Font.loadFont(WaiterApp.class.getResource("/fonts/fontawesome.ttf").toExternalForm(), 12);
 		primaryStage.setTitle("Waiter");
-		
+		primaryStage.getIcons().add(new Image("/resource/pomodoro16.png"));
+		primaryStage.getIcons().add(new Image("/resource/pomodoro32.png"));
 		Pane root = FXMLLoader.load(getClass().getResource("gui/waiterApp.fxml"));
-		
+
 		Scene scene = new Scene(root);
 		primaryStage.setScene(scene);
 		primaryStage.show();
